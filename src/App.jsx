7dc351107916f95b4691a466eeee95c8c6fc2a49
Radiat09 <rzsianboyridoy10@gmail.com>
2023-11-10 18:40:41 +0600

    basic set up with navbar footer

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-center">Test</h1>
+      <button className="btn btn-secondary">BBBB</button>
+    </>
+  );
+}
+
+export default App;
